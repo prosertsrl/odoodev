72d20600d67895fb90a25ca28b4216437e45b084
Angel Grullon <angelgrullon10@yahoo.com>
2023-07-06 14:22:05 -0400

    Agregando modulo peliculas

---
+++ apps/peliculas/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Modulo de peliculas',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'PROSERT SRL',
+    'category': 'Peliculas',
+    'website': 'https://prosertrd.com',
+    'description': 'Modulo para hacer presupuestos de peliculas',
+    'summary': 'Modulo de presupuesto para peliculas',
+    'data': ['']
+
+}
--- apps/votantes/__manifest__.py
+++ apps/votantes/__manifest__.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "votantes",
+    'name': "Votantes",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Aplicación para la gestion de votantes""",
 
     'description': """
-        Long description of module's purpose
+        Aplicación para la gestion de votantes
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "PROSERT SRL",
+    'website': "https://www.prosertrd.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '14.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
