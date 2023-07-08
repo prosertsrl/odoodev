010a881bd1fbb859af295931a37eb014dcc8f136
Angel Grullon <agrullon@prosertrd.com>
2023-07-08 10:03:49 -0400

    Modificaciones en el modulo Votantes

--- apps/votantes/__manifest__.py
+++ apps/votantes/__manifest__.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "votantes",
+    'name': "Gestion de Votantes",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Voter management module""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'author': "PROSERT SRL",
+    'website': "http://www.prosertrd.com",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
@@ -25,7 +17,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/car_views.xml',
+        'views/provinces_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
