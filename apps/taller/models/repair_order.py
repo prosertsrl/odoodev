8ee3824601b86b56f233e8d4233b24f99759fa0d
Angel Grullon <angelgrullon10@yahoo.com>
2023-07-19 20:11:56 -0400

    Agregando archivo reapair order

---
+++ apps/taller/models/repair_order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Car(models.Model):
+    _name = 'taller.repair.order'
+    _description = 'taller.repair'
+
+    customer_id = fields.Many2one(comodel_name="res.partner", string="Customer", required=True)
+
+    name = fields.Char(string="Name", default="New")
+    date = fields.Date(string="Date Order")
+
+    line_ids = fields.One2many(comodel_name="taller.repair.order.line", inverse_name="order_id")
